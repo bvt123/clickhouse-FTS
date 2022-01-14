57ee59a86b55a4a79cfc94c1e725f3d894ac3d3c
Boris Tyshkevich <bvt@itoolabs.com>
2022-01-14 13:42:56 +0300

    initial

--- indexer.sql
+++ indexer.sql
@@ -7,16 +7,20 @@ CREATE MATERIALIZED VIEW bvt.to_wiki TO bvt.wiki
 ) AS
 WITH
     '(\\d+)\\s(?:<feed>)?<doc><title>Wikipedia: (.+)</title><url>(.+)</url><abstract>(.+)</abstract><links></links></doc>' AS re,
-    extractAllGroupsVertical(row, re)[1] AS r
+    extractAllGroupsVertical(row, re)[1] AS r,
+    (select max(id) from wiki) as lastId
 SELECT
-    toUInt32OrZero(r[1]) AS id,
+    toUInt32OrZero(r[1])+lastId AS id,
     r[2] AS title,
     r[3] AS url,
     splitByNonAlpha(lower(r[4])) AS tokens
 FROM tmp1;
 
+/*
+индексация википедии
 grep -v \<sublink enwiki-latest-abstract.xml | sed -u 's#</doc>#</doc>\a#g' | tr -d '\n' | tr  '\a' '\n'  > 11 &
 cat -n 11 | head -20 | clickhouse-client -d bvt -u bvt -q "insert into tmp1 format LineAsString" --send_logs_level=debug
+*/
 
 insert into wIndex
 select arrayJoin(tokens) as token, groupBitmapState(id) as ids
--- search.sql
+++ search.sql
@@ -1,11 +1,20 @@
 
 select count() from wiki where id in
-    (select arrayJoin(bitmapToArray(groupBitmapAndState(ids))) from wIndex2 where token in ['green','wood'] limit 20);
+    (
+        select arrayJoin(bitmapToArray(groupBitmapAndState(ids))) from
+            (
+                select groupBitmapOrState(ids) as ids
+                from wIndex2
+                where token in ['green','wood']
+                group by token
+            )
+        limit 20
+    )
+;
 
 select count() from wiki where hasAll(tokens,['green','wood']);
 
 select count() from wiki2
 where id in
-    (select arrayJoin(bitmapToArray(ids)) from wIndex2 where token = 'wood' limit 20)
---where has(tokens,'wood')
+    (select arrayJoin(bitmapToArray(groupBitmapOrState(ids))) from wIndex2 where token = 'wood' limit 20)
 ;
